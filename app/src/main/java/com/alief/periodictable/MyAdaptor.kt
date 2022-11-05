cc95867425553dd0dbeed8fc73d2a2c50e026d2c
aliEsmaelFard <82215521+aliEsmaelFard@users.noreply.github.com>
2022-11-05 09:55:53 +0330

    complete MyAdaptor.kt

---
+++ app/src/main/java/com/alief/periodictable/MyAdaptor.kt
@@ -0,0 +1,81 @@
+package com.alief.periodictable
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.alief.periodictable.model.ElementItem
+
+class MyAdaptor(val listener: OnElementClickListener) :
+    RecyclerView.Adapter<MyAdaptor.ViewHolder>() {
+
+    private val differCallback = object : DiffUtil.ItemCallback<ElementItem>() {
+        override fun areItemsTheSame(oldItem: ElementItem, newItem: ElementItem): Boolean {
+            return oldItem.atomicNumber == newItem.atomicNumber
+        }
+
+        override fun areContentsTheSame(oldItem: ElementItem, newItem: ElementItem): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+    val differ = AsyncListDiffer(this, differCallback)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater.inflate(R.layout.elements_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        val element = differ.currentList[position]
+
+        holder.apply {
+            atomicNumber.text = element.atomicNumber.toString()
+            atomicMass.text = element.atomicMass
+            name.text = element.name
+            symbol.text = element.symbol
+
+            cardView.setCardBackgroundColor(Color.parseColor("#${element.cpkHexColor}"))
+        }
+
+
+    }
+
+    override fun getItemCount() = differ.currentList.size
+
+
+    inner class ViewHolder(itemView: View)
+        : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+
+        val symbol: TextView = itemView.findViewById(R.id.item_symbol)
+        val name: TextView = itemView.findViewById(R.id.item_name)
+        val atomicNumber: TextView = itemView.findViewById(R.id.item_atomic_num)
+        val atomicMass: TextView = itemView.findViewById(R.id.item_atomic_mass)
+        var cardView: CardView = itemView.findViewById(R.id.cardview)
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            val position = differ.currentList[bindingAdapterPosition]
+            listener.onClicked(position)
+        }
+
+
+    }
+}
+
+interface OnElementClickListener {
+    fun onClicked(elementItem: ElementItem)
+}
+
+}
